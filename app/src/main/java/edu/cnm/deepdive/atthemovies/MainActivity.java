32c67c3a418088cef3f9a8fee168bd8e717d11c2
matthewjones <matthewjones2435@gmail.com>
2019-06-12 17:06:02 -0600

    Worked on the databasing aspects of app

--- app/src/main/java/edu/cnm/deepdive/atthemovies/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/atthemovies/MainActivity.java
@@ -24,4 +24,11 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
   }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    MoviesViewModel viewModel = ViewModelProviders.of(this).get(MoviesViewModel.class);
+    viewModel.saveData(this);
+  }
 }
--- app/src/main/java/edu/cnm/deepdive/atthemovies/model/Movie.java
+++ app/src/main/java/edu/cnm/deepdive/atthemovies/model/Movie.java
@@ -1,19 +1,17 @@
 package edu.cnm.deepdive.atthemovies.model;
 
 import androidx.annotation.NonNull;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Movie {
+public class Movie implements Serializable {
 
-  private static Long last_id = 0L;
-
+  private static final long serialVersionUID = 1L;
+  public static Long last_id = 0L;
   private Long id;
-
   private String title;
-
   private List<Actor> actors = new ArrayList<>();
-
   private String screenwriter;
 
   public enum Genre {
@@ -65,7 +63,7 @@ public class Movie {
   @NonNull
   @Override
   public String toString() {
-    return title + ": " + genre;
+    return title + ": " + genre + " " + screenwriter;
   }
 }
 
